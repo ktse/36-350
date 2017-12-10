77f56484eeb1af53a584c552fcab3dd3a16db7c3
Kendrick Tse <kendricktse888@gmail.com>
2017-12-10 01:24:25 -0500

    Add files via upload

---
+++ alien message.R
@@ -0,0 +1,5 @@
+cat("Hello! I am from a planet Earth. Earth is inhabited by intelligent lifeforms called Humans.")
+cat("As humans some believe there is extrateresstrial life, some believe there is a higher being.")
+cat("If you are reading this and not from Earth, are you ... god?")
+cat("If you are, please bless my grades for this semester. Amen.")
+
